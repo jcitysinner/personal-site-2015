abf5ac018bd84e712984f61355c49f5a687cbdd6
jcitysinner <jessesinfield@Jesses-MacBook-Pro.local>
2015-10-09 22:08:24 -0400

    Init Commit / Structure

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php include( "header_doc.php"); ?>
+   <body class="">
+<?php include( "header.php"); ?>
+
+
+  <div class='left-section'>
+    <div class='main-col absolute-center'>
+      <svg class='main-logo'><use xlink:href="#main-logo"></use></svg>
+      <h1>Jesse Sinfield</h1>
+      <h2>User-Experience Developer</h2>
+      <nav>
+        <a href='#about'>About</a>
+        <a href='#portfolio'>Portfolio</a>
+        <a href='#contact'>Contact</a>
+      </nav>
+    </div>
+  </div>
+
+  <div class='right-section'>
+    <div class='main-col'>
+
+      <div class='about section' id='about'>
+        <h3>About</h3>
+        <div class='image-card'>
+          <img src='img/raster/compressed/me.jpg'>
+          <span class='caption'>Me. Working on this website. On a train.</span>
+        </div>
+        <p>I build experiences. That’s the goal from the first line of code to the last closed issue. If I can take a beautiful design and turn it into a pixel-perfect experience for the user, then I feel like I’ve done my job. I use JavaScript, SASS, and HTML5 to create gorgeous and engaging experiences that keep the user coming back every time.</p>
+        <p>I dig beards and hip hop. I play video games. I read (not as much as a should) and I write (also…not as much as I should). I’m a simple guy who just wants to build cool stuff and change the world.</p>
+      </div>
+
+      <div class='portfolio section' id='section'>
+        <h3>Portfolio</h3>
+        <div class='image-card whole-click'>
+          <img src='img/raster/compressed/sharp.jpg'>
+          <a href='#' class='caption'>01. SHARP Magazine</a> 
+        </div>
+        <h4>Role: <span>Front End Developer</span></h4>
+        <h4>Tools: <span>HTML5, SASS, JavaScript, PHP, Grunt</span></h4>
+        <p>SHARP Magazine is by far one of my favorite projects. This project was undertaken by the agency I work at, Exsite Inc. This project was so fun to work on because the layout and design was so clean and beautiful and it really felt like we were creating something special.</p>
+
+        <div class='image-card whole-click'>
+          <img src='img/raster/compressed/grandstand.jpg'>
+          <a href='#' class='caption'>02. Grandstand</a> 
+        </div>
+        <h4>Role: <span>Front End Developer</span></h4>
+        <h4>Tools: <span>HTML5, SASS, JavaScript, PHP, Grunt</span></h4>
+        <p>Grandstand is another project that I built at Exsite Inc. This fantastic sports-highlight sharing app needed an equally fantastic website. This landing page is what we ended up with after a few different versions. I love the typeography that our designer <a href='http://www.marcjenkinson.com/' target='_blank'>Marc Jenkinson</a> came up with. But I gotta say, the coolest part has to be the two videos that are sync’d up. </p>
+        
+        <div class='image-card whole-click'>
+          <img src='img/raster/compressed/kwissa.jpg'>
+          <a href='#' class='caption'>03. Em Kwissa</a> 
+        </div>
+        <h4>Role: <span>Front / Back-End Developer, Designer</span></h4>
+        <h4>Tools: <span>Sketch, HTML5, LESS, JavaScript, PHP, Wordpress</span></h4>
+        <p>Em Kwissa’s website is one of my proudest achievments, not just because I was able to work with a very close friend on it, but also because I was able to do everything on this project. Design, Front-End Dev, and Back-End Dev. This project is very dear to me, it’s kind of like a pet project. I imagine by the time I redesign my website again, Em’s will have gone through at least a couple more facelifts!</p>
+      </div>
+
+      <div class='contact section' id='contact'>
+        <h3>Contact</h3>
+        <p>Whew! That was a lot about me. Well, if after all that you want to have a chat, here’s a few ways to get at me! Even if you’re not looking to hire, feel free to message me! I’m very chatty.</p>
+        <a href='http://www.twitter.com/jcitysinner' class='contact-link'><svg class='twitter-icon'><use xlink:href="#twitter-icon"></use></svg><span>www.twitter.com/jcitysinner</span></a>
+        <a href='http://ca.linkedin.com/in/jessesinfield' class='contact-link'><svg class='linkedin-icon'><use xlink:href="#linked-icon"></use></svg><span>ca.linkedin.com/in/jessesinfield</span></a>
+        <a href='mailto:jessesinfield@gmail.com' class='contact-link'><svg class='email-icon'><use xlink:href="#email-icon"></use></svg><span>jessesinfield@gmail.com</span></a>
+      </div>
+
+    </div>
+  </div>
+
+
+<?php include( "footer.php"); ?>
